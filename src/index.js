e760b3aee38ab30657e546541eb3c4d05b9ecd7c
jetlime <paul.houssel@yahoo.de>
2021-11-24 19:22:45 +0100

    add the token as an environment variable

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS] });
+const dotenv = require("dotenv")
+dotenv.config()
 // Token is saved as an environment model in the linux server machine
-const token = PROCESS.env.token ;
-
+const token = process.env.DISCORD_TOKEN ;
 client.login(token);
 
 client.on('ready', () => {
