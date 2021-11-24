7699fdeb2237426a461328b4dffc72dc42e7a1b1
jetlime <paul.houssel@yahoo.de>
2021-11-24 18:39:09 +0100

    Conection discord using a token

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+// Token is saved as an environment model in the linux server machine
+const token = PROCESS.env.token ;
+
+client.login(token);
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('message', msg => {
+    if(msg.content === 'ping') {
+        msg.reply("Pong!")
+        msg.react('😄');
+    }
+    
+});
